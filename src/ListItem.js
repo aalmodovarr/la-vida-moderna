9d6999209958fe591f651db943d3e5eedc3e9ad5
Antonio <a.almodovar@apploading.com>
2018-11-02 19:52:32 +0100

    Adds card ui element to each row item

--- src/ListItem.js
+++ src/ListItem.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import PlayArrowIcon from '@material-ui/icons/PlayArrow';
 import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 
 class ListItem extends Component {
     constructor(props) {
@@ -20,20 +22,24 @@ class ListItem extends Component {
 
         return (
             <li >
-                <div className="ListItem-background">
-                    <div className="ListItem-main-column">
-                        <h3 className="title">{this.props.description}</h3>
-                        <h4 className="subtitle">{this.props.name}</h4>
-                    </div>
-                    <div className="ListItem-info-column" style={{ position: "relative" }}>
-                        <h6>{pubDate}</h6>
-                        <Button variant="fab" mini="true" style={{ position: "absolute", bottom: "10px", right: "10px" }}>
-                            <PlayArrowIcon
-                                className="play-button"
-                                onClick={() => this.play(this.props.link)} />
-                        </Button>
-                    </div>
-                </div>
+                <Card >
+                    <CardContent style={{ padding: "10px" }} >
+                        <div className="ListItem-background">
+                            <div className="ListItem-main-column">
+                                <h3 className="title">{this.props.description}</h3>
+                                <h4 className="subtitle">{this.props.name}</h4>
+                            </div>
+                            <div className="ListItem-info-column" style={{ position: "relative" }}>
+                                <h6>{pubDate}</h6>
+                                <Button variant="fab" mini="true" style={{ position: "absolute", bottom: "10px", right: "10px" }}>
+                                    <PlayArrowIcon
+                                        className="play-button"
+                                        onClick={() => this.play(this.props.link)} />
+                                </Button>
+                            </div>
+                        </div>
+                    </CardContent>
+                </Card>
             </li >
         );
     }
